8bb7d0f24e6898dbeb3728cd180fa1013e161e11
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-21 00:15:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+		if(num==1)
+		return 0;
+	else if(num==2)
+		return 1;
+		
+	var current_indx=3
+	var a = 0;
+	var b = 1
+	var c;
+	while(num!=current_indx-1){
+	    c=a+b;
+	    a=b;
+	    b=c
+	    current_indx++;
+	}
+	return c;
 }
 
 module.exports = fibonacci;
